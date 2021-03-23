890947ce4444f588691e42c9ab2857e35b5f974d
Abuoblie <Abu.oblie@gmail.com>
2021-03-23 21:58:45 +0100

    index bugfixed

--- Assets/phpHeader.php
+++ Assets/phpHeader.php
@@ -1,7 +1,7 @@
 <header>
         <nav>
                 <ul class="navlist">
-                        <li><a href="./index.php">Home</a></li>
+                        <li><a href="..\index.php">Home</a></li>
                         <li><a href="">product</a></li>
                         <li><a href="">After sales service</a></li>
                         <li><a href="">contact</a></li>
